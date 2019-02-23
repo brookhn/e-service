1ac141716dc759b0bbc405a41f1a1af16a1a2845
yangtao <18111987@suningcultural.com>
2019-02-23 18:33:23 +0800

    test

--- src/main/java/com/pp/server/Distribution/TokenBucket.java
+++ src/main/java/com/pp/server/Distribution/TokenBucket.java
@@ -1,5 +1,27 @@
 package com.pp.server.Distribution;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class TokenBucket {
+
+    private static final int DEFAULT_BUCKET_SIZE = 60*1024*1024;
+
+    private int everyTokenSize = 1;
+
+    private int maxFlowRate;
+
+    private int avgFlowRate;
+
+    private ArrayBlockingQueue<Byte> tokenQueue = new ArrayBlockingQueue<>(DEFAULT_BUCKET_SIZE);
+
+    private ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+
+    private volatile boolean started = false;
+
+    private ReentrantLock lock = new ReentrantLock(true);
+
     
 }
